0a8c7131753f0f4e4a0f224e8aec24280e3ff810
OXI <52327373+oxi-eth@users.noreply.github.com>
2022-03-01 19:16:05 +0300

    Update airdrop.py

--- airdrop.py
+++ airdrop.py
@@ -5,7 +5,7 @@ import subprocess
 from os import listdir
 from os.path import isfile, join
 
-mintAdress = "" # your custom token contract address
+mintAddress = "" # your custom token contract address
 
 fileName = "address.csv"
 
@@ -18,8 +18,8 @@ with open(fileName, 'r', encoding='utf-8') as f:
 
         line_count += 1
 
-        print(f'spl-token transfer --allow-unfunded-receipt --fund-receipient {mintAdress} {row[1]} {row[0]}')
-        transferInstr = subprocess.run(["spl-token", "transfer", "--allow-unfunded-receipent", "--fund-receipent", mintAdress, row[1], row[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print(f'spl-token transfer --allow-unfunded-receipt --fund-receipient {mintAddress} {row[1]} {row[0]}')
+        transferInstr = subprocess.run(["spl-token", "transfer", "--allow-unfunded-receipent", "--fund-receipent", mintAddress, row[1], row[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         wf = open("txn-log.txt", "a")
         wf.write(transferInstr.stdout + transferInstr.stderr)
